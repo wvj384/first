898ce1b84a3495e4935cab806688279018cb3089
root <root@vm7813.(none)>
2013-11-12 12:34:36 +0400

    MY FIRST COMMIT

--- src/parse_csv.erl
+++ src/parse_csv.erl
@@ -3,7 +3,22 @@
 
 parse(Path) -> 
 	{ok, Binary}=file:read_file(Path),
-%	Binary. 
-	[parse_p(binary:split(X,<<",">>,[global])) || X <- binary:split(Binary,<<"\n">>,[global])].
+%	io:format("~w",[Binary]), 
+	[parse_p(X) || X <- binary:split(Binary,<<"\n">>,[global])].
 
-parse_p(List) -> [binary_to_list(X) || X <- List].
+parse_p(X) ->
+	List=binary:split(X,<<",">>,[global]), 
+	list_to_tuple([ get_type(Y) || Y <- List]).
+
+get_type(<<"\"",Rest/binary>>) -> 
+	Size=size(Rest)-1, 
+	<<String:Size/binary,_>>=Rest, 
+	binary_to_list(String); 
+get_type(B) ->
+	List_B=binary_to_list(B), 
+	try list_to_integer(List_B) of
+	I -> I
+	catch
+	error:_ -> catch list_to_float(List_B)
+
+end.
